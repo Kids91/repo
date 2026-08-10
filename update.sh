004f15de1735a831d979824a651f0b3fc535c498
KidsDev <95279366+Kids91@users.noreply.github.com>
2026-08-10 08:48:41 +0700

    Refresh repo

--- update.sh
+++ update.sh
@@ -1,5 +1,20 @@
-dpkg-scanpackages -m ./debs > Packages
+#!/bin/bash
+# ✅ Vào thư mục repo
+
+echo Scan tất cả .deb files trong thư mục deb/
+dpkg-scanpackages -m ./debs /dev/null > Packages
+
+echo Nén thành Packages.gz
 bzip2 -c Packages > Packages.bz2
+
+echo Nén thành Packages.bz2
 gzip -c Packages > Packages.gz
 
+echo "MD5Sum:" >> Release
+md5sum Packages.gz >> Release
+
+echo "SHA256:" >> Release
+sha256sum Packages.gz >> Release
+
+echo ✅ Done.
 cd ..
